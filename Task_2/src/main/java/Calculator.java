9e35bf7bdcce73d05198d17dcfe3c1b89db75822
alextyamakov <alex.tyamakov@gmail.com>
2020-11-22 23:00:37 +0300

    First release

---
+++ Task_2/src/main/java/Calculator.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Выберете операцию: \n1 - сложение\n2 - умножение\n3 - вычитание\n4 - деление:\n");
+        int choice = in.nextInt();
+        System.out.println("Введите первый аргумент:");
+        double arg1 = in.nextDouble();
+        System.out.println("Введите второй аргумент:");
+        double arg2 = in.nextDouble();
+        double result = 0;
+        switch (choice) {
+            case 1:
+                result = arg1 + arg2;
+                break;
+            case 2:
+                result = arg1 * arg2;
+                break;
+            case 3:
+                result = arg1 - arg2;
+                break;
+            case 4:
+                result = arg1 / arg2;
+                break;
+        }
+        System.out.printf("Результат операции: %.4f", result);
+    }
+}
