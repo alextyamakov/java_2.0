9738f6846936b10dee069a222707277463df9caf
alextyamakov <alex.tyamakov@gmail.com>
2020-11-24 19:14:23 +0300

    Third commit

---
+++ Task_4/src/main/java/Candies/Candy_bars.java
@@ -0,0 +1,23 @@
+package Candies;
+
+public class Candy_bars extends All_kinds{
+    public String size;
+
+    public Candy_bars(String name, double weight, double price, String size) {
+        super(name, weight, price);
+        this.size = size;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    @Override
+    public String toString() {
+        return "Candy_bars {" + super.toString() + ", size='" + size + '}';
+    }
+}
